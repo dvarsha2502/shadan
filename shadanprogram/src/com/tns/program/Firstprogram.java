b6c60d477c8c01fc9356b287b766fb8d35efdf62
padma <padma@Padmavathi>
2024-06-19 23:46:56 +0530

    commit message

--- shadanprogram/src/com/tns/program/Firstprogram.java
+++ shadanprogram/src/com/tns/program/Firstprogram.java
@@ -1,9 +1,10 @@
 package com.tns.program;
 
-public class Shadanprogram {
+public class Firstprogram {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to java");
+
 
 	}
 
---
+++ shadanprogram/src/com/tns/program/Secondprogram.java
@@ -0,0 +1,8 @@
+package com.tns.program;
+
+public class Secondprogram {
+	public static void main(String[] args) {
+		System.out.println("hello everyone");
+	}
+}
+
