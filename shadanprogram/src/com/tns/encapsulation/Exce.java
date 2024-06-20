dcd23adfe5eed834657d8c4d07881e4dc53d256c
padma <padma@Padmavathi>
2024-06-20 08:55:14 +0530

    commit message

---
+++ shadanprogram/src/com/tns/encapsulation/Exce.java
@@ -0,0 +1,15 @@
+package com.tns.encapsulation;
+
+public class Exce {
+
+	public static void main(String[] args) {
+		    Encapsulation e=new Encapsulation();
+		    e.serialnumber=1;
+		    e.name="java";
+		    e.age=24;
+		    
+		    e.show();//accessing the member function or method
+
+	}
+	                 
+}
--- shadanprogram/src/com/tns/packagesprogram/Publicexample.java
+++ shadanprogram/src/com/tns/packagesprogram/Publicexample.java
@@ -2,7 +2,7 @@ package com.tns.packagesprogram;
 
 public class Publicexample {
 
-	int a=9;
+	public int a=9;
 	protected char f='y';
 	public static void main(String[] args) {
 		Publicexample u=new Publicexample();
